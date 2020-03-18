61dc4524836524c206844b3440c95fd607db192f
Robin McAndrew <rlmcandrew@agiledigital.com.au>
2020-03-19 09:05:21 +1100

    CSC-1047/ADS-870 = node-raml-mocker: rRevert Prettier changes

--- app/ramlmock-api.js
+++ app/ramlmock-api.js
@@ -1,73 +1,69 @@
-const express = require("express");
-const mung = require("express-mung");
-const mockService = require("osprey-mock-service");
-const osprey = require("osprey");
-const parser = require("raml-1-parser");
-const uuidv4 = require("uuid/v4");
-const { startApp } = require("./start-app");
+const express = require('express');
+const mung = require('express-mung');
+const mockService = require('osprey-mock-service');
+const osprey = require('osprey');
+const parser = require('raml-1-parser');
+const uuidv4 = require('uuid/v4');
+const { startApp } = require('./start-app');
 
 exports.ramlmocker = async (port, ramlFile, transformers) => {
-  // Initialise the response transformers
-  var responseTransformers = transformers
-    ? transformers.map(transformer => {
+
+    // Initialise the response transformers
+    var responseTransformers = transformers ? transformers.map(transformer => {
         return {
-          ...transformer,
-          id: uuidv4()
-        };
-      })
-    : [];
+            ...transformer,
+            id: uuidv4()
+        }
+    }) : [];
 
-  const remove = id =>
-    (responseTransformers = responseTransformers.filter(t => t.id !== id));
+    const remove = (id) => responseTransformers = responseTransformers.filter(t => t.id !== id);
 
-  const add = transformer => responseTransformers.push(transformer);
+    const add = (transformer) => responseTransformers.push(transformer);
 
-  const get = id =>
-    responseTransformers.find(transformer => transformer.id === id);
+    const get = (id) => responseTransformers.find(transformer => transformer.id === id);
 
-  const list = () => responseTransformers;
+    const list = () => responseTransformers;
 
-  const app = express();
+    const app = express();
 
-  // Intercept the RAML generated response and hand off to the transformers
-  app.use(
-    mung.write((chunk, encoding, request, response) => {
-      responseTransformers.forEach(element => {
-        console.log(element);
-      });
+    // Intercept the RAML generated response and hand off to the transformers
+    app.use(mung.write((chunk, encoding, request, response) => {
 
-      const matchesPath = transformer =>
-        !transformer.path || transformer.path === request.route.path;
+        responseTransformers.forEach(element => {
+            console.log(element)
+        });
 
-      console.log(request.route.path);
+        const matchesPath = transformer => !transformer.path || transformer.path === request.route.path;
 
-      const reducer = (body, transformer) =>
-        transformer.transform(body, request, response) || body;
+        console.log(request.route.path);
 
-      return responseTransformers.filter(matchesPath).reduce(reducer, chunk);
-    })
-  );
+        const reducer = (body, transformer) => transformer.transform(body, request, response) || body
 
-  const ramlApi = await parser.loadRAML(ramlFile, {
-    rejectOnErrors: true
-  });
+        return responseTransformers.filter(matchesPath).reduce(reducer, chunk);    
+    }));
 
-  var raml = ramlApi.expand(true).toJSON({
-    serializeMetadata: false
-  });
-
-  app.use(osprey.server(raml, { RAMLVersion: "RAML10" }));
-  app.use(mockService(raml));
-
-  return new Promise((resolve, reject) => {
-    startApp(port, app, () => {
-      console.log(`RAML mock server running on [${port}].`);
-      resolve({
-        remove,
-        add,
-        get,
-        list
-      });
+    const ramlApi = await parser.loadRAML(ramlFile, {
+        rejectOnErrors: true
     });
-  });
+
+    var raml = ramlApi.expand(true).toJSON({
+        serializeMetadata: false
+    });
+
+    app.use(osprey.server(raml, { RAMLVersion: 'RAML10' }));
+    app.use(mockService(raml));
+
+
+
+    return new Promise((resolve, reject) => {
+        startApp(port, app, () => {
+            console.log(`RAML mock server running on [${port}].`);
+            resolve({
+                remove,
+                add,
+                get,
+                list
+            });
+        });
+    });
 };
