baa1ed168d62d98a30f7c3c1319bfd29a5405e09
Shouvojit Sarker <sxsarker@agiledigital.com.au>
2020-11-03 09:05:17 +1100

    fix(jest.config.js): Remove dead code

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,4 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  // preset: "ts-jest",
-  // coverageReporters: ["json", "lcov", "text", "clover", "cobertura"],
-  // reporters: [
-  //   "default",
-  //   [
-  //     "jest-junit",
-  //     {
-  //       outputDirectory: "<rootDir>/coverage",
-  //       usePathForSuiteName: "true",
-  //     },
-  //   ],
-  // ],
 };
